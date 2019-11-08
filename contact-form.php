e1f7bcb02ed720ab065979a5a7992d5a85f24495
dw82-dev <55720979+dw82-dev@users.noreply.github.com>
2019-11-08 19:20:04 +1300

    Test upload for GitHub pages
    Test upload for GitHub pages

---
+++ contact-form.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Contact Email: <?php echo $_POST["email"]; ?><br>
+Subject: <?php echo $_POST["subject"]; ?><br><br>
+Message: <?php echo $_POST["message"]; ?><br>
+
+</body>
+</html>
